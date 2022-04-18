3353014cef44d0fddb1700bee4283a96bad3d997
石井将司 <optacos@gmail.com>
2022-04-18 19:18:41 +0900

    UPDATE: FontAwesomeIconの導入

--- backend/laravel/resources/js/Pages/Welcome.vue
+++ backend/laravel/resources/js/Pages/Welcome.vue
@@ -1,5 +1,11 @@
 <script setup>
 import { Head, Link } from '@inertiajs/inertia-vue3';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPaintbrush } from '@fortawesome/free-solid-svg-icons';
+import { faComment } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faPaintbrush, faComment);
 
 defineProps({
     canLogin: Boolean,
@@ -86,8 +92,8 @@ defineProps({
                                     viewBox="0 0 24 24"
                                     class="w-8 h-8 text-gray-500"
                                 ><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" /></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold">
-                                    <a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Documentation</a>
+                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">
+                                    Documentation
                                 </div>
                             </div>
 
@@ -101,17 +107,9 @@ defineProps({
 
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
                             <div class="flex items-center">
-                                <svg
-                                    fill="none"
-                                    stroke="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    viewBox="0 0 24 24"
-                                    class="w-8 h-8 text-gray-500"
-                                ><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" /><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
+                                <FontAwesomeIcon icon="paintbrush" class="w-8 h-8 text-gray-500" />
                                 <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">
-                                    Real Time Paint Chat
+                                    Real Time Paint
                                 </div>
                             </div>
 
@@ -124,15 +122,7 @@ defineProps({
 
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700">
                             <div class="flex items-center">
-                                <svg
-                                    fill="none"
-                                    stroke="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    viewBox="0 0 24 24"
-                                    class="w-8 h-8 text-gray-500"
-                                ><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" /></svg>
+                                <FontAwesomeIcon icon="comment" class="w-8 h-8 text-gray-500" />
                                 <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">
                                     Real Time Chat
                                 </div>
