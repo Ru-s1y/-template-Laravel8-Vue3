297ebbb6218f06a52c2aedc185a4c895022bb87f
石井将司 <optacos@gmail.com>
2022-04-20 13:58:09 +0900

    UPDATE: Welcomeページのデザイン修正

--- backend/laravel/resources/js/Pages/Welcome.vue
+++ backend/laravel/resources/js/Pages/Welcome.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { Head, Link } from '@inertiajs/inertia-vue3';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPaintbrush } from '@fortawesome/free-solid-svg-icons';
-import { faComment } from '@fortawesome/free-solid-svg-icons';
+import { faPaintbrush, faComment, faNewspaper } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faPaintbrush, faComment);
+library.add(
+    faPaintbrush,
+    faComment,
+    faNewspaper,
+);
 
 defineProps({
     canLogin: Boolean,
@@ -83,15 +86,7 @@ defineProps({
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
                             <div class="flex items-center">
-                                <svg
-                                    fill="none"
-                                    stroke="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    viewBox="0 0 24 24"
-                                    class="w-8 h-8 text-gray-500"
-                                ><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" /></svg>
+                                <FontAwesomeIcon icon="newspaper" class="w-8 h-8 text-gray-500" />
                                 <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">
                                     Documentation
                                 </div>
@@ -99,8 +94,7 @@ defineProps({
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    なんか文字入れとけ
-                                    長ったらしい説明文が入るわ
+                                    ブログというかキュレーションサイトみたいなのを作る予定
                                 </div>
                             </div>
                         </div>
--- backend/laravel/resources/js/components/Welcome.vue
+++ backend/laravel/resources/js/components/Welcome.vue
@@ -6,7 +6,15 @@
     <div>
         <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
             <TitleLogo />
-            Hello Welcome Page
+            キュレーションサイト作っていくよ!<br>
+            ここがヘッダーになる予定
+        </div>
+        <div>
+            <p>ここに記事の一覧が入るよ</p>
+            <p>ここに記事の一覧が入るよ</p>
+            <p>ここに記事の一覧が入るよ</p>
+            <p>ここに記事の一覧が入るよ</p>
+            <p>ここに記事の一覧が入るよ</p>
         </div>
     </div>
 </template>
